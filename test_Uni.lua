252a7cbe64677bcae30176c2d2992df50b21a39f
Leon170312 <leonmatti1703@gmail.com>
2024-07-04 04:28:51 -0700

    Update test_Uni.lua

--- test_Uni.lua
+++ test_Uni.lua
@@ -102,18 +102,3 @@ end)
          game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
    end,
 })
-
-local Dropdown = MainTab:CreateDropdown({
-   Name = "Choose",
-   Options = {"Infinite yeild", "Dark dex"},
-   CurrentOption = "Infinite yeild",
-   MultipleOptions = false, -- Only one option can be selected at a time
-   Flag = "DropdownChoose", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Option)
-      if Option == "Infinite yeild" then
-         loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/infiniteyield/master/source.lua", true))()
-      elseif Option == "Dark dex" then
-         loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
-      end
-   end,
-})
