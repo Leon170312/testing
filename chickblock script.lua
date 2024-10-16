ade02ac6548adc3c80393145fb978bac57059d28
Leon170312 <leonmatti1703@gmail.com>
2024-10-16 11:09:20 -0700

    Update chickblock script.lua

--- chickblock script.lua
+++ chickblock script.lua
@@ -103,7 +103,7 @@ local Window = Rayfield:CreateWindow({
               end
           elseif Input.KeyCode == Enum.KeyCode.X then
               _G.AimbotEnabled = not _G.AimbotEnabled
-              local message = _G.AimbotEnabled and "Aimbot On" or "Aimbot Off"
+              local message = _G.AimbotEnabled and "Nigger" or "Nigga"
               StarterGui:SetCore("SendNotification", {
                   Title = "Notification";
                   Text = message;
@@ -390,3 +390,52 @@ end
 checkPlayerInventories()
     end,
  })
+
+local Button = MainTab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+   local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+ 
+game.StarterGui:SetCore("SendNotification", {
+    Title = "made by JN HH Gaming";
+ 
+   
+})
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "have fun killing murderer";
+    Text = "Enjoy"; -- what the text says (ofc)
+    Duration = 60;
+})
+_G.HeadSize = 20
+_G.Disabled = true
+
+game:GetService('RunService').RenderStepped:connect(function()
+if _G.Disabled then
+for i,v in next, game:GetService('Players'):GetPlayers() do
+if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+pcall(function()
+v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+v.Character.HumanoidRootPart.Transparency = 0.7
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+v.Character.HumanoidRootPart.Material = "Neon"
+v.Character.HumanoidRootPart.CanCollide = false
+end)
+end
+end
+end
+end)
+   end,
+})
